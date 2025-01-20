538e32ea4d36ee243d563aa7961e9273fbfee365
mbaangel <95703191+mbaangel@users.noreply.github.com>
2025-01-19 18:29:00 -0600

    Create timeOutTimers.js

---
+++ timeOutTimers.js
@@ -0,0 +1,21 @@
+const df = require("durable-functions");
+const moment = require("moment");
+
+module.exports = df.orchestrator(function*(context) {
+    const deadline = moment.utc(context.df.currentUtcDateTime).add(30, "s");
+
+    const activityTask = context.df.callActivity("GetQuote");
+    const timeoutTask = context.df.createTimer(deadline.toDate());
+
+    const winner = yield context.df.Task.any([activityTask, timeoutTask]);
+    if (winner === activityTask) {
+        // success case
+        timeoutTask.cancel();
+        return true;
+    }
+    else
+    {
+        // timeout case
+        return false;
+    }
+});
